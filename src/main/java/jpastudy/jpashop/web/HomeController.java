0e65ab783155321564fdfc847a795e3829a345b0
hanminjung <hanminjung_@naver.com>
2021-10-20 16:55:04 +0900

    HomeController 추가

---
+++ src/main/java/jpastudy/jpashop/web/HomeController.java
@@ -0,0 +1,25 @@
+package jpastudy.jpashop.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+    @RequestMapping("/frag")
+    public String home(){
+        log.info("home Controller");
+        return "home";
+    }
+
+    //메인 화면 만들기
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+
+
+}
